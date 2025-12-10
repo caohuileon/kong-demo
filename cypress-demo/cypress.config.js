95a9726ec3c687391512758f47596cab8a924129
leon <caohuileon@163.com>
2025-12-10 13:15:59 +0800

    Signed-off-by: leon <caohuileon@163.com>

--- cypress-demo/cypress.config.js
+++ cypress-demo/cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8001',
+    supportFile: false,
     setupNodeEvents(on, config) {
       return config;
     },
---
+++ cypress-demo/cypress/support/e2e.js
@@ -0,0 +1,9 @@
+// cypress/support/e2e.js
+// 这是 Cypress 的全局支持文件
+// 可以在这里添加自定义命令和全局配置
+
+// 例如：添加自定义命令
+// Cypress.Commands.add('login', (email, password) => { ... })
+
+// 导入自定义命令（如果有的话）
+// import './commands'
